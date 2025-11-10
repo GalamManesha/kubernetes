59ff87a46c567e5632b7e87afee38bfc77d3a1cf
GalamManesha <maneeshagalam689@gmail.com>
2025-11-10 18:05:44 +0530

    Create variables.tf

---
+++ terraform-jenkins/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-0a64f837ea2469df6"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "instance_count" {
+  default = 3
+}
+
+variable "project_name" {
+  default = "manesha"
+}
