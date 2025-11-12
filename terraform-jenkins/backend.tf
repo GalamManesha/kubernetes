5eee899e2b0a02b119ed257a6925de17ee2d9ac6
GalamManesha <maneeshagalam689@gmail.com>
2025-11-12 15:07:33 +0530

    Update backend.tf

--- terraform-jenkins/backend.tf
+++ terraform-jenkins/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket" 
-    key            = "jenkins/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
-    encrypt        = true
-  }
-}
+
