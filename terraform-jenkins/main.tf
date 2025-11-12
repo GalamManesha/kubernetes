6c7f6db64c6eedc861f451f86e523b03734123bb
GalamManesha <maneeshagalam689@gmail.com>
2025-11-12 15:06:31 +0530

    Update main.tf

--- terraform-jenkins/main.tf
+++ terraform-jenkins/main.tf
@@ -1,23 +1,9 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-# 3 EC2 Instances
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "example" {
   count         = var.instance_count
   ami           = var.ami_id
   instance_type = var.instance_type
 
   tags = {
-    Name = "${var.project_name}-ec2-${count.index + 1}"
-  }
-}
-
-# 1 S3 Bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "${var.project_name}-terraform-bucket"
-
-  tags = {
-    Name = "Terraform S3 Bucket"
+    Name = "jenkins-terraform-ec2-${count.index + 1}"
   }
 }
