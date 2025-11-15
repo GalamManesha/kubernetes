d2cfe5b6714adc0baabc78b3ec30f3c702edf931
GalamManesha <maneeshagalam689@gmail.com>
2025-11-15 15:06:37 +0530

    Update main.tf

--- terra/main.tf
+++ terra/main.tf
@@ -1,1 +1,9 @@
+resource "aws_instance" "example" {
+  count         = var.instance_count
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
+  tags = {
+    Name = "jenkins-terraform-ec2-${count.index + 1}"
+  }
+}
