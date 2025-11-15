ff678a65e374704ae3303504707ec911cd9b139e
GalamManesha <maneeshagalam689@gmail.com>
2025-11-15 15:03:45 +0530

    Delete terraform-jenkins directory

--- terraform-jenkins/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "example" {
-  count         = var.instance_count
-  ami           = var.ami_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "jenkins-terraform-ec2-${count.index + 1}"
-  }
-}
